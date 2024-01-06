35bb3f28ddbf7401ec3ee64074a28eecfcc01f44
Shayan Shaikh <shayanshaikh996@gmail.com>
2024-01-06 18:02:04 +0200

    Ready to go live

---
+++ script/script.js
@@ -0,0 +1,9 @@
+
+// You can add additional JavaScript functionality or animations using Anime.js or other libraries
+anime({
+    targets: ".invitation-card",
+    translateY: [-50, 0],
+    opacity: [0, 1],
+    duration: 1000,
+    easing: "easeInOutQuad",
+});
